f2f5a06c993bd4c7a68ef6a7d2bda0ef9706adc3
Criogentleman <43676883+Criogentleman@users.noreply.github.com>
2018-11-25 20:36:31 +0200

    Create 11.2.py

---
+++ Part_II/11.2.py
@@ -0,0 +1,18 @@
+from file_as_str import file_as_str
+from parse_cdp_neighbors import parse_cdp_neighbors
+from draw_network_graph import draw_topology
+
+'''SW1 = parse_cdp_neighbors(file_as_str('C:\\Users\\Samir\\Desktop\\Trash code\\11.2a\\sh_cdp_n_sw1.txt'))
+R1 = parse_cdp_neighbors(file_as_str('C:\\Users\\Samir\\Desktop\\Trash code\\11.2a\\sh_cdp_n_r1.txt'))
+R2 = parse_cdp_neighbors(file_as_str('C:\\Users\\Samir\\Desktop\\Trash code\\11.2a\\sh_cdp_n_r2.txt'))
+R3 = parse_cdp_neighbors(file_as_str('C:\\Users\\Samir\\Desktop\\Trash code\\11.2a\\sh_cdp_n_r3.txt'))
+SW1.update(R1)
+SW1.update(R2)
+SW1.update(R3)
+
+result = {}
+for key, value in SW1.items():
+    if value not in result.values():
+        result[value] = key'''
+
+draw_topology(parse_cdp_neighbors(file_as_str('sw1_sh_cdp_neighbors.txt')))
