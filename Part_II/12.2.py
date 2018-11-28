abec29eb6350adf4b8bb4ba24f3721cbd9720acf
Criogentleman <43676883+Criogentleman@users.noreply.github.com>
2018-11-28 10:07:55 +0200

    Create 12.2.py

---
+++ Part_II/12.2.py
@@ -0,0 +1,31 @@
+import subprocess
+
+def ping_script(ip_list):
+    reachable = []
+    unreachable = []
+    ip1_list = []
+    for ip in ip_list:
+        if '-' in ip:
+            ip = ip.split('-')
+            first = [int(x) for x in ip[0].split('.')]
+            if not '.' in ip[-1]:
+                for first[-1] in range(first[-1], int(ip[-1]) + 1):
+                    ip1_list.append('.'.join([str(x) for x in first]))
+            else:
+                last = [int(x) for x in ip[-1].split('.')]
+                for first[-1] in range(first[-1], last[-1] + 1):
+                    ip1_list.append('.'.join([str(x) for x in first]))
+        else:
+            ip1_list.append(ip)
+
+    for ip in ip1_list:
+        ping = subprocess.run(['ping', ip, '-n', '3'], stdout=subprocess.DEVNULL)  # DEVNULL to hide process
+        if ping.returncode == 0:
+            reachable.append(ip)
+        else:
+            unreachable.append(ip)
+    return reachable, unreachable
+
+ip_list = ['8.8.4.4', '8.8.8.1-8.8.8.8', '193.122.94.1-5']
+
+print(ping_script(ip_list))
